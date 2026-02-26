5d79944eb4a3adf9e073417ca5742acdd99b3594
gy9vin <Gy9vin@gmail.com>
2026-02-26 21:40:28 +0300

    feat: self-host шрифты и Telegram SDK для работы при белых списках

--- src/components/TelegramLoginButton.tsx
+++ src/components/TelegramLoginButton.tsx
@@ -27,7 +27,7 @@ export default function TelegramLoginButton({
 
     // Create script element for Telegram Login Widget
     const script = document.createElement('script');
-    script.src = 'https://telegram.org/js/telegram-widget.js?22';
+    script.src = '/js/telegram-widget.js';
     script.setAttribute('data-telegram-login', botUsername);
     script.setAttribute('data-size', 'large');
     script.setAttribute('data-radius', '8');
